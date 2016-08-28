2876e8a69acb95a748879da4625af62714f12738
mrmgreen <m.r.green@hotmail.co.uk>
2016-08-28 16:50:50 +0100

    added flexbox

--- client/EditingArea/index.jsx
+++ client/EditingArea/index.jsx
@@ -1,11 +1,12 @@
 import React, { Component, PropTypes } from 'react'
 import WebPage from './webpage/WebPage'
 import ToolBar from './toolbar/Toolbar'
+import styles from './EditingArea.local.css'
 
 class EditingArea extends Component {
   render() {
     return(
-      <div>
+      <div className={ styles['EditingArea'] }>
         <ToolBar />
         <WebPage />
       </div>
