5291e81dea62f77a5cb38814c020e4ad6dc42d1e
mrmgreen <m.r.green@hotmail.co.uk>
2016-08-28 16:28:59 +0100

    added toolbar placeholder css

--- client/toolbar/Toolbar.jsx
+++ client/toolbar/Toolbar.jsx
@@ -1,10 +1,11 @@
 import React, { Component, PropTypes } from 'react'
+import styles from './toolbar.local.css'
 import Text from './Text'
 
 class Toolbar extends Component {
   render() {
     return (
-      <div>
+      <div className={styles['toolbar']}>
         <h2>This is a toolbar</h2>
         <Text />
       </div>
