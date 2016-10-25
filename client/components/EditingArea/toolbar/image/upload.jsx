52e8b82a5334c3a91d15478be4c4b77a698e6ea7
mrmgreen <m.r.green@hotmail.co.uk>
2016-10-25 08:59:27 +0100

    adding files

--- client/components/EditingArea/toolbar/image/upload.jsx
+++ client/components/EditingArea/toolbar/image/upload.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import styles from '../toolbar.local.css'
+import { round } from 'lodash'
 
 class Image extends Component {
   constructor(props) {
@@ -8,7 +9,23 @@ class Image extends Component {
   }
 
   handleChange(e) {
-    console.log('e.target.files || e.target.value ===', e.target.files);
+    console.log('e.target.files || e.target.value ===', e.target.files || e.target.value);
+    const fileList = e.target.files || e.target.value;
+    this.printFileSize(fileList);
+  }
+
+  printFileSize(fileList) {
+    for (let i = 0; i < fileList.length; i++) {
+      console.log('fileList item ===', fileList[i]);
+      const bytes = fileList[i].size;
+      const KB = round(bytes / 1024, 2);
+      const MB = round(KB / 1024, 2);
+      if (MB > 2.3) {
+        console.log('The image is too big, please try reducing its size');
+      } else {
+        console.log('uploading');
+      }
+    }
   }
 
   render() {
@@ -16,11 +33,11 @@ class Image extends Component {
       <div className={ styles['toolbarComponent'] }>
         <h3>Upload image</h3>
         <input
+          multiple
           type="file"
           name="file"
           id="file"
           onChange={this._handleChange}
-          ref={(node) => { this.node = node }}
          />
       </div>
     )
