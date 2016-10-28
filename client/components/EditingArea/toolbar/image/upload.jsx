04a0ec4dbb26af0c31a47ab36939d2d8501e8a5f
mrmgreen <m.r.green@hotmail.co.uk>
2016-10-28 19:13:51 +0100

    added classnames and swapped class depending on active state in upload component

--- client/components/EditingArea/toolbar/image/upload.jsx
+++ client/components/EditingArea/toolbar/image/upload.jsx
@@ -1,14 +1,34 @@
 import React, { Component, PropTypes } from 'react'
 import styles from '../toolbar.local.css'
+import cx from "classnames";
 
 class Image extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    !(newProps.image.src === this.props.image.src) && !(newProps.image.src === '') ?
+    this.setState({
+      active: true,
+    })
+    : this.setState({
+      active: false,
+    })
+  }
 
   render() {
     let img = this.props.image.src ? <img src={this.props.image.src}></img> : null;
+    let className = cx(
+      this.state.active ? [styles.imagepreviewActive] : [styles.imagepreview],
+    );
     return (
       <div className={ styles['toolbarComponent'] }>
         <h3>Upload image</h3>
-        <div className={styles['imagepreview']}>
+        <div className={ className }>
           {img}
         </div>
         <input
