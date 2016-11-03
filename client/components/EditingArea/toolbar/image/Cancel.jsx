49ebfd7ce1a59f8ef752819e5909047453133c8f
mrmgreen <m.r.green@hotmail.co.uk>
2016-11-03 08:56:21 +0000

    adding a cancel button to the image preview

---
+++ client/components/EditingArea/toolbar/image/Cancel.jsx
@@ -0,0 +1,8 @@
+import React, { PropTypes } from 'react';
+import styles from '../toolbar.local.scss';
+
+function Cancel(props) {
+  return <button className={ styles.cancel }>Cancel</button>
+}
+
+export default Cancel;
--- client/components/EditingArea/toolbar/image/ImgPreviewHeader.jsx
+++ client/components/EditingArea/toolbar/image/ImgPreviewHeader.jsx
@@ -1,8 +1,14 @@
 import React, { PropTypes } from 'react';
-import styles from '../toolbar.local.scss'
+import styles from '../toolbar.local.scss';
+import Cancel from './Cancel';
 
 function ImgPreviewHeader(props) {
-  return <div className={styles.imgPreviewHeader}>ImgPreviewHeader</div>
+  return (
+    <div className={styles.imgPreviewHeader}>
+      ImgPreviewHeader
+      <Cancel />
+    </div>
+  )
 }
 
 export default ImgPreviewHeader;
